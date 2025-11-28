b63e9851d0420a8d6c69656cba21d371a141e071
Luis Calleja <luiscallejaenrique@gmail.com>
2025-11-28 19:04:00 -0300

    feat: create RosaryEnd.jsx page and write a comment to describe it

---
+++ src/pages/RosaryEnd.jsx
@@ -0,0 +1,10 @@
+/*
+* ./src/pages/RosaryEnd.jsx
+* This is a page where the user goes when he or she
+* completes the Rosary. Here, the user can write a
+* personalized prayer or intentions.
+* This page also shows the last step of the Rosary
+* (the Hail Holy Queen).
+*
+* The user will be redirected here using React Router.
+*/
