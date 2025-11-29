47664f52568f3d7c1397f55ab0a11c1ef593262a
Luis Calleja <luiscallejaenrique@gmail.com>
2025-11-29 15:07:28 -0300

    "feat: create Home.jsx page - This page is the home page where the user can start Praying. - Make App.jsx more static by moving all the logic to Home.jsx. - It doesn't have more text yet."

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,22 @@
-import { useTranslation } from "react-i18next";
 import LanguageSwitcher from "./components/LanguageSwitcher";
 import './styles/App.css';
 import { Route, Routes } from 'react-router';
 import RosaryPage from "./pages/RosaryPage.jsx"
 import RosaryEnd from "./pages/RosaryEnd";
+import HomePage from "./pages/Home.jsx"
 
 
 function App() {
-  const { t } = useTranslation();
 
-  const handleStartPrayer = () => {
-    const mainContent = document.querySelector('.main-content');
-    if (mainContent) {
-      mainContent.style.display = 'block';
-    }
-  }
 
   return (
     <>
-      <div>
-        <h1>{t("home.title")}</h1>
-        <button className="start-prayer" onClick={handleStartPrayer}>{t("home.startButton")}</button>
-      </div>
-
       <LanguageSwitcher />
-
-      <div className="main-content" style={{display: 'none'}}> 
-        <RosaryPage />
-      </div>
       <Routes>
-        <Route path="/" element={<RosaryPage />} />
-        <Route path="/end" element={<RosaryEnd />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/Rosary" element={<RosaryPage />} />
+        <Route path="/Rosary/end" element={<RosaryEnd />} />
       </Routes>
-
     </>
   )
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import { useTranslation } from "react-i18next";
+import { NavLink } from "../components/NavLink.jsx";
+
+export default function HomePage() {
+    const { t } = useTranslation();
+
+    return (
+        <main>
+            <h1>{t("home.title")}</h1>
+            <h2>{t("home.subtitle")}</h2>
+
+            <NavLink to="/Rosary" className="start-button">
+                {t("home.startButton")}
+            </NavLink>
+        </main>
+    );
+}
