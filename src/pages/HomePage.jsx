1d1049b860e62ad91b738104f7449f05a6975873
Luis Calleja <luiscallejaenrique@gmail.com>
2025-12-02 17:57:45 -0300

    fix(style): rename info-button and start-button to info-link and start-link to improve readability

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,11 +10,11 @@ export default function HomePage() {
             <h2>{t("home.subtitle")}</h2>
             <p>{t("overview.intro")}</p>
 
-            <NavLink to="/Rosary" className="start-button">
+            <NavLink to="/Rosary" className="start-link">
                 {t("home.startButton")}
             </NavLink>
 
-            <NavLink to="/info" className="info-button">
+            <NavLink to="/info" className="info-link">
                 {t("home.infoButton")}
             </NavLink>
         </main>
