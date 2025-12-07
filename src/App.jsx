a8670000c89d2e0ac4fa002f35c8229ae1e7a788
Luis Calleja <luiscallejaenrique@gmail.com>
2025-12-07 12:38:51 -0300

    "refactor: use lazy loading in App.jsx - Import lazy and Suspense to show a message when the content is loading. - Create new translations for the waiting message."

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,35 @@
 import './styles/App.css';
+import { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router';
-import RosaryPage from "./pages/RosaryPage.jsx"
-import RosaryEnd from "./pages/RosaryEnd";
-import HomePage from "./pages/HomePage.jsx"
-import NotFoundPage from "./pages/NotFoundPage.jsx";
-import RosaryInfoPage from "./pages/RosaryInfoPage.jsx";
-import MainLayout from './layout/MainLayout.jsx';
+import { useTranslation } from 'react-i18next';
+
+const RosaryPage = lazy(() => import('./pages/RosaryPage.jsx'))
+const RosaryEnd = lazy(() => import('./pages/RosaryEnd.jsx'))
+const HomePage = lazy(() => import('./pages/HomePage.jsx'))
+const NotFoundPage = lazy(() => import('./pages/NotFoundPage.jsx'))
+const RosaryInfoPage = lazy(() => import('./pages/RosaryInfoPage.jsx'))
+const MainLayout = lazy(() => import('./layout/MainLayout.jsx'))
 
 function App() {
+  const { t } = useTranslation();
+
   return (
-    <>
-      <Routes>
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/info" element={<RosaryInfoPage />} />
-          <Route path="/Rosary" element={<RosaryPage />} />
-          <Route path="/Rosary/end" element={<RosaryEnd />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Route>
-      </Routes>
-    </>
+      <Suspense fallback={<div style={{ maxWidth: '1280px', margin: '0 auto', padding: '0 1rem' }}> {t("home.waiting")} </div>} >
+        
+        <Routes>
+          <Route element={<MainLayout />}>
+          
+            <Route path="/" element={<HomePage />} />
+            <Route path="/info" element={<RosaryInfoPage />} />
+            <Route path="/Rosary" element={<RosaryPage />} />
+            <Route path="/Rosary/end" element={<RosaryEnd />} />
+            <Route path="*" element={<NotFoundPage />} />
+          
+          </Route>
+        </Routes>
+
+      </Suspense>
   )
 }
+
 export default App
