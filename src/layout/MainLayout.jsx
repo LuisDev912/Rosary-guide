503e1b918c77df219b01c1d3c8dca491fd45edd1
Luis Calleja <luiscallejaenrique@gmail.com>
2025-12-20 17:26:43 -0300

    feat(accessibility): create a button for skip to content

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,12 +1,24 @@
 import { Outlet } from "react-router";
 import { Header, Footer } from "../components/Semantic.jsx";
+import { useTranslation } from "react-i18next";
 
 function MainLayout() {
+    const { t } = useTranslation();
+    
     return (
         <>
             <Header />
 
-            <main>
+            <button
+                className="skip-link"
+                onClick={() => {
+                    document.getElementById('main-content')?.focus()
+                }}
+            >
+                {t("a11y.skipToContent")}
+            </button>
+
+            <main id="main-content" tabIndex='-1'>
                 <Outlet />
             </main>
 
