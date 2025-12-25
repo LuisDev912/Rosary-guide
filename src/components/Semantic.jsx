e1667a74889e01a0931e1d25ae1c44a1953b5fee
Luis <luiscallejaenrique@gmail.com>
2025-12-25 14:20:23 -0300

    chore: bump version to v1.3.0

--- src/components/Semantic.jsx
+++ src/components/Semantic.jsx
@@ -11,7 +11,7 @@ export function Footer() {
 
             <div className={SemanticStyles.footerInfo}>
                 © {new Date().getFullYear()} {t("labels.footerMessage")} — Luis Calleja <br />
-                <small className={SemanticStyles.version}>v1.2.0</small>
+                <small className={SemanticStyles.version}>v1.3.0</small>
             </div>
         </footer>
     );
