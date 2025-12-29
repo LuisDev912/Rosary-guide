3dc4284deab11eeabc31bea99ba21f6c6c04f00b
Luis <luiscallejaenrique@gmail.com>
2025-12-29 14:38:38 -0300

    feat(refactor): improve 404 page by removing duplicated code

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,32 +1,25 @@
-import { NavLink } from "../components/NavLink";
+import {HomeIcon} from '../assets/icons/HomeIcon.jsx';
 import { useTranslation } from "react-i18next";
 
 export default function NotFoundPage() {
     const { t } = useTranslation();
 
-    const pages = t("notFound.pages", { returnObjects: true });
-    const links = [
-        "/",
-        "/Rosary",
-        "/Rosary/end",
-        "/info"
-    ];
-
     return (
-        <section>
+        <section
+            style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    flexDirection: 'column'
+                }}
+            >
             <h1 className="heading-primary">{t("notFound.title")}</h1>
-            <h3>{t("notFound.alternatives")}</h3>
 
-            <ul>
-                {pages.map((pageLabel, index) => (
-                    <li key={index}>
-                        <NavLink to={links[index]}>
-                            {pageLabel}
-                        </NavLink>
-                    </li>
-                ))}
-                {/* TODO: refactor duplicated code in NotFoundPage page and Header component */}
-            </ul>
+            <button
+                
+                onClick={() => navigate('/')}>
+                {t("buttons.goBack")}
+                <HomeIcon />
+            </button>
         </section>
-    )
-}
+    );
+};
