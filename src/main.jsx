a7daa575c3e3012a329786a9a52422fa2c06edb8
Luis Calleja <luiscallejaenrique@gmail.com>
2026-02-27 11:20:02 -0300

    feat(styles): create a new utilities.css file

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
-import './styles/App.css'
-import './i18n.js'
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.jsx';
+import './styles/App.css';
+import './styles/utilities.css';
+import './i18n.js';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
